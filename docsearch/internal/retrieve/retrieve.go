fbb0e2b96c49f196bb4d1e79308e3f70731460c8
ValeriaErS <valeriaeroxina2004@mail.ru>
2026-07-06 15:36:34 +0300

    Update retrieve.go

--- docsearch/internal/retrieve/retrieve.go
+++ docsearch/internal/retrieve/retrieve.go
@@ -1,43 +1,56 @@
 package retrieve
-
 import "math"
 
-func Search(texts []string, vectors [][]float64, query []float64, TopK int) ([]string, []float64){
-    scores := []float64{} //находим topK самых похожих+даем оценку
+func Search(texts []string, docs []string, vectors [][]float64, query []float64, TopK int) ([]string, []string, []float64) { // возвращает тексты, имена документов и оценки
+    scores := []float64{}
+    resultTexts := []string{}
+    resultDocs := []string{}
+    resultScores := []float64{}
 
-    for i := 0; i < len(vectors); i++ {
-        scores = append(scores, similarity(query, vectors[i]))
+
+    for i := 0; i < len(vectors); i++ {  // считаю похожесть для каждого вектора
+        sim := similarity(query, vectors[i])
+        scores = append(scores, sim)
+        resultTexts = append(resultTexts, texts[i])
+        resultDocs = append(resultDocs, docs[i])
+        resultScores = append(resultScores, sim)
     }
 
-    for i := 0; i < len(scores); i++ {  //сортировка по убыв.
+   
+    for i := 0; i < len(scores); i++ {      // сортирую по убыванию самые похожие будут первыми, меняю местами
         for j := i + 1; j < len(scores); j++ {
             if scores[i] < scores[j] {
                 scores[i], scores[j] = scores[j], scores[i]
-                texts[i], texts[j] = texts[j], texts[i]
+                resultTexts[i], resultTexts[j] = resultTexts[j], resultTexts[i]
+                resultDocs[i], resultDocs[j] = resultDocs[j], resultDocs[i]
+                resultScores[i], resultScores[j] = resultScores[j], resultScores[i]
             }
         }
     }
 
-   resultTexts := []string{}
-   resultScores := []float64{}
-    for i := 0; i < TopK && i < len(texts); i++ {
-        resultTexts = append(resultTexts, texts[i])
-        resultScores = append(resultScores, scores[i])
+    finalTexts := []string{}  // беру первые TopK
+    finalDocs := []string{}
+    finalScores := []float64{}
+    for i := 0; i < TopK && i < len(resultTexts); i++ {
+        finalTexts = append(finalTexts, resultTexts[i])
+        finalDocs = append(finalDocs, resultDocs[i])
+        finalScores = append(finalScores, resultScores[i])
     }
-    return resultTexts, resultScores
+
+    return finalTexts, finalDocs, finalScores
 }
 
-func similarity(a []float64, b []float64) float64 { //считаю косинус.близ.между векторами
+func similarity(a []float64, b []float64) float64 {  // считает косинусную близость между двумя векторами
     if len(a) != len(b) {
         return 0
     }
 
-    dot := 0.0 //скалярное произведение
+    dot := 0.0   // скалярное произведение
     for i := 0; i < len(a); i++ {
         dot = dot + a[i]*b[i]
     }
 
-    lenA := 0.0 //длина векторов
+    lenA := 0.0     // длины векторов
     lenB := 0.0
     for i := 0; i < len(a); i++ {
         lenA = lenA + a[i]*a[i]
