0109d22ec9ecfac351c4c31ce40e8ba78481383c
ValeriaErS <valeriaeroxina2004@mail.ru>
2026-07-22 09:32:22 +0300

    fix: JWT секрет вынесен в .env

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -1,20 +1,33 @@
 package auth
 import(
+	"os"
 	"time"
     "github.com/golang-jwt/jwt/v4"
 )
-var secretKey=[]byte("my-secret-key")
+
 func MakeToken(username string)(string,error){  // Создаю токен
-data:=jwt.MapClaims{
+secret := []byte(os.Getenv("JWT_SECRET"))
+    if len(secret) == 0 {
+        secret = []byte("fallback-secret-key")
+    }
+
+	claims := jwt.MapClaims{
 	"user":username,
 	"exp":time.Now().Add(time.Hour * 24).Unix(),
 }	
-token:=jwt.NewWithClaims(jwt.SigningMethodHS256,data)
-return token.SignedString(secretKey)
+token:=jwt.NewWithClaims(jwt.SigningMethodHS256,claims)
+return token.SignedString(secret)
 }
+
+
 func CheckToken(tokenString string)(string,error){ // Проверяю токен и достаю имя пользователя
+	secret := []byte(os.Getenv("JWT_SECRET"))
+    if len(secret) == 0 {
+        secret = []byte("fallback-secret-key")
+    }
+	
 	token,err:=jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
-        return secretKey, nil
+        return secret, nil
 })
 if err!=nil{
 	return "",err
@@ -23,9 +36,9 @@ claims,ok:=token.Claims.(jwt.MapClaims)
 if !ok || !token.Valid{
 	 return "", jwt.ErrInvalidKey
 }
-name,ok:=claims["user"].(string) // Беру имя пользователя
+username,ok:=claims["user"].(string) // Беру имя пользователя
 if !ok{
 	 return "", jwt.ErrInvalidKey
 }
-return name,nil
+return username,nil
 }
